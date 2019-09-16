3716a19b6292537c0132829c87f891d755b44922
Gabriel Ferreira <gabrielferrer@outlook.com.br>
2019-09-16 13:24:07 -0300

    remove obsolete function buffer

--- request/rabbitmq/request.ts
+++ request/rabbitmq/request.ts
@@ -1,13 +1,11 @@
 export default class RabbitMQRepository {
     removeStore(params) {
-        console.log('removeStore');
-
         global.rabbitmq.createChannel(function (err, ch) {
             const msg = JSON.stringify(params);
 
             const q = 'remove-store';
             ch.assertQueue(q, {durable: false});
-            ch.sendToQueue(q, new Buffer(msg));
+            ch.sendToQueue(q, Buffer.from(msg));
 
             console.log(" [x] Sent %s", msg);
         });
